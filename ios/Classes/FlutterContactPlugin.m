2c76f5fb370bf53d0f7c57466ebdab88153cdc27
庞熠明 <995637308@qq.com>
2019-07-25 19:13:22 +0800

    新增iOS获取，联系人，iOS，android获取权限

--- ios/Classes/FlutterContactPlugin.m
+++ ios/Classes/FlutterContactPlugin.m
@@ -1,26 +1,129 @@
 #import "FlutterContactPlugin.h"
+#import <Contacts/Contacts.h>
 
 @implementation FlutterContactPlugin
+
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  FlutterMethodChannel* channel = [FlutterMethodChannel
-      methodChannelWithName:@"flutter_contact_plugin"
-            binaryMessenger:[registrar messenger]];
-  FlutterContactPlugin* instance = [[FlutterContactPlugin alloc] init];
-  [registrar addMethodCallDelegate:instance channel:channel];
+    FlutterMethodChannel* channel = [FlutterMethodChannel
+                                     methodChannelWithName:@"flutter_contact_plugin"
+                                     binaryMessenger:[registrar messenger]];
+    FlutterContactPlugin* instance = [[FlutterContactPlugin alloc] init];
+    [registrar addMethodCallDelegate:instance channel:channel];
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-  if ([@"getPlatformVersion" isEqualToString:call.method]) {
-    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
-  } else {
-    result(FlutterMethodNotImplemented);
-  }
+    if ([@"getPlatformVersion" isEqualToString:call.method]) {
+        result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+    } else  if([@"getContactInfo" isEqualToString:call.method]){
+        [self requestContactAuthorAfterSystemVersion9:result];
+        
+    }else {
+        result(FlutterMethodNotImplemented);
+    }
+    
 }
-- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-  if ([@"getContactInfo" isEqualToString:call.method]) {
-    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
-  } else {
-    result(FlutterMethodNotImplemented);
-  }
+//请求通讯录权限
+#pragma mark 请求通讯录权限
+- (void)requestContactAuthorAfterSystemVersion9:(FlutterResult)result{
+    
+    CNAuthorizationStatus status = [CNContactStore authorizationStatusForEntityType:CNEntityTypeContacts];
+    if (status == CNAuthorizationStatusNotDetermined) {
+        CNContactStore *store = [[CNContactStore alloc] init];
+        [store requestAccessForEntityType:CNEntityTypeContacts completionHandler:^(BOOL granted, NSError*  _Nullable error) {
+            if (error) {
+                NSLog(@"授权失败");
+            }else {
+                NSLog(@"成功授权");
+            }
+        }];
+    }
+    else if(status == CNAuthorizationStatusRestricted)
+    {
+        NSLog(@"用户拒绝");
+        [self showAlertViewAboutNotAuthorAccessContact];
+    }
+    else if (status == CNAuthorizationStatusDenied)
+    {
+        NSLog(@"用户拒绝");
+        [self showAlertViewAboutNotAuthorAccessContact];
+    }
+    else if (status == CNAuthorizationStatusAuthorized)//已经授权
+    {
+        //有通讯录权限-- 进行下一步操作
+        
+        [self openContact: result];
+    }
+    
+}
+
+//有通讯录权限-- 进行下一步操作
+- (void)openContact:(FlutterResult)result{
+    // 获取指定的字段,并不是要获取所有字段，需要指定具体的字段
+    NSArray *keysToFetch = @[CNContactGivenNameKey, CNContactFamilyNameKey, CNContactPhoneNumbersKey];
+    CNContactFetchRequest *fetchRequest = [[CNContactFetchRequest alloc] initWithKeysToFetch:keysToFetch];
+    CNContactStore *contactStore = [[CNContactStore alloc] init];
+    
+    [contactStore enumerateContactsWithFetchRequest:fetchRequest error:nil usingBlock:^(CNContact * _Nonnull contact, BOOL * _Nonnull stop) {
+        NSLog(@"-------------------------------------------------------");
+        
+        NSString *givenName = contact.givenName;
+        NSString *familyName = contact.familyName;
+        NSLog(@"givenName=%@, familyName=%@", givenName, familyName);
+        //拼接姓名
+        NSString *nameStr = [NSString stringWithFormat:@"%@%@",contact.familyName,contact.givenName];
+        
+        NSArray *phoneNumbers = contact.phoneNumbers;
+        
+        NSString *contactInfo =@"[";
+        //        NSMutableArray *arrayInfo =[[NSMutableArray alloc] init];
+        //        CNPhoneNumber  * cnphoneNumber = contact.phoneNumbers[0];
+        
+        //        NSString * phoneNumber = cnphoneNumber.stringValue;
+        
+        for (CNLabeledValue *labelValue in phoneNumbers) {
+            //遍历一个人名下的多个电话号码
+            NSString *label = labelValue.label;
+            //   NSString *    phoneNumber = labelValue.value;
+            CNPhoneNumber *phoneNumber = labelValue.value;
+            
+            NSString * string = phoneNumber.stringValue ;
+            
+            //去掉电话中的特殊字符
+            string = [string stringByReplacingOccurrencesOfString:@"+86" withString:@""];
+            string = [string stringByReplacingOccurrencesOfString:@"-" withString:@""];
+            string = [string stringByReplacingOccurrencesOfString:@"(" withString:@""];
+            string = [string stringByReplacingOccurrencesOfString:@")" withString:@""];
+            string = [string stringByReplacingOccurrencesOfString:@" " withString:@""];
+            string = [string stringByReplacingOccurrencesOfString:@" " withString:@""];
+            
+            NSLog(@"姓名=%@, 电话号码是=%@", nameStr, string);
+            NSString *oc =[NSString stringWithFormat: @"{\"name\":%@,\"phone\":%@},",nameStr, string];
+            contactInfo=  [contactInfo stringByAppendingString:oc];
+        }
+        contactInfo = [contactInfo substringToIndex:[contactInfo length]-1];
+        [contactInfo stringByAppendingString:@"]"];
+        NSLog(@"%@", contactInfo);
+        result(contactInfo);
+        //    *stop = YES; // 停止循环，相当于break；
+        
+    }];
+    
+}
+
+
+
+
+//提示没有通讯录权限
+- (void)showAlertViewAboutNotAuthorAccessContact{
+    
+    UIAlertController *alertController = [UIAlertController
+                                          alertControllerWithTitle:@"请授权通讯录权限"
+                                          message:@"请在iPhone的\"设置-隐私-通讯录\"选项中,允许花解解访问你的通讯录"
+                                          preferredStyle: UIAlertControllerStyleAlert];
+    
+    UIAlertAction *OKAction = [UIAlertAction actionWithTitle:@"好的" style:UIAlertActionStyleDefault handler:nil];
+    [alertController addAction:OKAction];
+    
+    //    [self presentViewController:alertController animated:YES completion:nil];
 }
 @end
